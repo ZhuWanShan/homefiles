e3d3a974c6ebcfbcfdcc77ef78423aca97ec97cb
Spark Zhu <szhu.spark@gmail.com>
2017-07-20 12:03:50 +0800

    basic vim

--- bin/install.sh
+++ bin/install.sh
@@ -1,11 +1,41 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 # ==== Functions ==========
 
 print_help(){
     echo "Help"
+
 }
 
-# =========================
+home(){
+    local DOT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
+    echo $DOT_HOME
+}
 
-echo `print_help`
+install_vim(){
+    echo "---------------Instal Vim Config -----------------"
+    mkdir -p ~/.vim
+    echo "cp $DOT_HOME/vim/vimrc ~/.vimrc"
+    cp $DOT_HOME/vim/vimrc ~/.vimrc
+    echo "cp -R $DOT_HOME/vim/autoload ~/.vim"
+    cp -R $DOT_HOME/vim/autoload ~/.vim
+    echo "---------------Finshed-----------------"
+}
+# =======================
+
+
+
+# ============ Variables ===================
+
+DOT_HOME=`home`
+
+# ==========================================
+
+case $1 in
+    vim)
+        install_vim
+        ;;
+    *)
+        print_help
+        ;;
+esac
